cdf3d407a45e15ce59454ed8ee82426d8cf22ddd
luck07051 <luck0751@gmail.com>
2022-12-15 01:34:59 +0800

    first commit

---
+++ config.h
@@ -0,0 +1,11 @@
+#define CMDLENGTH 45
+#define DELIMITER "  "
+#define CLICKABLE_BLOCKS
+
+const Block blocks[] = {
+	BLOCK("sb-pacman",  0,    24),
+	BLOCK("sb-uptime",  5,    23),
+	BLOCK("sb-mem",     5,    22),
+	BLOCK("sb-vol",     0,    21),
+	BLOCK("sb-date",    1,    20),
+};
