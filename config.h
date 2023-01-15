d6685dd8f72c2e28a4767dde7bba6ab48ee40d43
luck07051 <luck0751@gmail.com>
2023-01-16 01:54:11 +0800

    config.

--- config.h
+++ config.h
@@ -2,10 +2,15 @@
 #define DELIMITER "  "
 #define CLICKABLE_BLOCKS
 
+/* update block my run `kill -((34+signal)) $(pidof dwmblocks)` */
+
 const Block blocks[] = {
-	BLOCK("sb-pacman",  0,    24),
-	BLOCK("sb-uptime",  5,    23),
-	BLOCK("sb-mem",     5,    22),
-	BLOCK("sb-vol",     0,    21),
-	BLOCK("sb-date",    1,    20),
+	    /* command     interval signal */
+	BLOCK("sb-bookmark",  0,    27),
+	BLOCK("sb-pacman",    0,    24),
+	BLOCK("sb-vpn",       0,    25),
+	BLOCK("sb-uptime",    5,    23),
+	BLOCK("sb-mem",       5,    22),
+	BLOCK("sb-vol",       0,    21),
+	BLOCK("sb-date",      1,    20),
 };
