3e620a5ec909ea50b9c7d26e2c7ddd65cd8d7f74
luck07051 <luck0751@gmail.com>
2024-02-26 21:09:14 +0800

    config

--- config.h
+++ config.h
@@ -6,13 +6,11 @@
 
 const Block blocks[] = {
 	    /* command     interval signal */
-	BLOCK("sb-pacman",    0,    28),
-	BLOCK("sb-vpn",       0,    27),
-	BLOCK("sb-uptime",    5,    26),
-	BLOCK("sb-mem",       5,    25),
-	BLOCK("sb-brightness",0,    24),
-	BLOCK("sb-battery",   5,    23),
-	BLOCK("sb-vol",       0,    22),
-	BLOCK("sb-network",   0,    21),
+	BLOCK("sb-pacman",    0,    27),
+	BLOCK("sb-tailscale", 5,    28),
+	BLOCK("sb-uptime",    5,    25),
+	BLOCK("sb-ibus",      5,    26),
+	BLOCK("sb-mem",       5,    24),
+	BLOCK("sb-vol",       0,    21),
 	BLOCK("sb-date",      5,    20),
 };
